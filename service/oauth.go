ba21b38c321db98b601c85e6402f4daeedb2fb83
luyb177 <2085661244@qq.com>
2025-12-26 17:45:55 +0800

    refactor:删除了旧的刷新token的内容
    Signed-off-by: luyb177 <2085661244@qq.com>

--- service/oauth.go
+++ service/oauth.go
@@ -1,25 +1,15 @@
 package service
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"sync"
-	"time"
 
 	"github.com/muxi-Infra/FeedBack-Backend/config"
-	"github.com/muxi-Infra/FeedBack-Backend/errs"
 	"github.com/muxi-Infra/FeedBack-Backend/pkg/feishu"
 
 	"golang.org/x/oauth2"
 )
 
 type AuthService interface {
-	StartAutoRefresh(accessToken string, refreshToken string, t time.Duration)
-	AutoRefreshToken() error
-
 	StartRefresh(accessToken string, refreshToken string)
 	GetAccessToken() string
 	GetTenantAccessToken() string
@@ -31,7 +21,6 @@ type AuthServiceImpl struct {
 	mutex        sync.RWMutex
 	accessToken  string
 	refreshToken string // 不对外展示
-	first        bool   // 是否为第一次设定
 
 	tenantAccessToken string // 自建应用发送消息需要使用的 token
 }
@@ -50,7 +39,6 @@ func NewOauth(c config.ClientConfig) AuthService {
 		},
 		accessToken:       "",
 		refreshToken:      "",
-		first:             true,
 		tenantAccessToken: "",
 	}
 }
@@ -79,118 +67,6 @@ func (o *AuthServiceImpl) StartRefresh(accessToken string, refreshToken string) 
 
 	var tenantAuto feishu.AutoRefresher
 	tenantAuto.Start(tenantRefresher)
-}
-
-// StartAutoRefresh 启动定时刷新协程
-func (o *AuthServiceImpl) StartAutoRefresh(accessToken string, refreshToken string, t time.Duration) {
-	o.mutex.Lock()
-	o.accessToken = accessToken
-	o.refreshToken = refreshToken
-	o.mutex.Unlock()
-
-	if o.first == true {
-		go func(t time.Duration) {
-			o.mutex.Lock()
-			o.first = false
-			o.mutex.Unlock()
-			ticker := time.NewTicker(t)
-			defer ticker.Stop()
-			for {
-				<-ticker.C
-				err := o.AutoRefreshToken()
-				if err != nil {
-					// TODO log
-					fmt.Printf("refresh token failed:%v\n", err)
-				}
-			}
-		}(t)
-	}
-}
-
-func (o *AuthServiceImpl) AutoRefreshToken() error {
-	token := o.getRefreshToken()
-
-	requestBody := map[string]string{
-		"grant_type":    "refresh_token",
-		"refresh_token": token,
-		"client_id":     o.oauthConfig.ClientID,
-		"client_secret": o.oauthConfig.ClientSecret,
-	}
-	jsonBody, _ := json.Marshal(requestBody)
-
-	// 调用飞书 token 接口
-	resp, err := http.Post(
-		"https://open.feishu.cn/open-apis/authen/v2/oauth/token",
-		"application/json; charset=utf-8",
-		bytes.NewBuffer(jsonBody),
-	)
-	if err != nil {
-		return errs.FeishuRequestError(err)
-	}
-	defer resp.Body.Close()
-	// 解析响应体
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return errs.ReadResponseError(err)
-	}
-
-	// 解析 JSON
-	var tokenResp map[string]interface{}
-	if err = json.Unmarshal(body, &tokenResp); err != nil {
-		return errs.DeserializationError(err)
-	}
-
-	accessToken, ok1 := tokenResp["access_token"].(string)
-	refreshToken, ok2 := tokenResp["refresh_token"].(string)
-	// 更新缓存
-	if ok1 && ok2 {
-		o.mutex.Lock()
-		o.accessToken = accessToken
-		o.refreshToken = refreshToken
-		o.mutex.Unlock()
-	}
-
-	// 返回 token 数据
-	return nil
-}
-
-func (o *AuthServiceImpl) AutoRefreshTenantToken() error {
-	requestBody := map[string]string{
-		"app_id":     o.oauthConfig.ClientID,
-		"app_secret": o.oauthConfig.ClientSecret,
-	}
-
-	jsonBody, _ := json.Marshal(requestBody)
-
-	// 发起请求
-	resp, err := http.Post("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
-		"application/json; charset=utf-8",
-		bytes.NewBuffer(jsonBody),
-	)
-	if err != nil {
-		return errs.FeishuRequestError(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return errs.ReadResponseError(err)
-	}
-
-	// 解析响应
-	var res map[string]interface{}
-	if err = json.Unmarshal(body, &res); err != nil {
-		return errs.DeserializationError(err)
-	}
-
-	tenantAccessToken, ok := res["tenant_access_token"].(string)
-	if ok {
-		o.mutex.Lock()
-		o.tenantAccessToken = tenantAccessToken
-		o.mutex.Unlock()
-	}
-
-	return nil
 }
 
 func (o *AuthServiceImpl) GetAccessToken() string {
