cc3ef432471a01f018930f8f99187b181a9d79fc
Gaofeng Jiang <jiang040906@gmail.com>
2025-12-31 16:50:27 +0800

    feat: reorganize error codes and update README for improved clarity

--- errs/business.go
+++ errs/business.go
@@ -11,23 +11,23 @@ import (
 */
 
 const (
-	RecordNotFoundCode        = 200000 + iota // 记录不存在
+	TokenGeneratedErrorCode   = 200000 + iota // Token 生成失败
+	TableIdentifyNotFoundCode                 // 表格标识未找到
+	RecordNotFoundCode                        // 记录不存在
 	AddPendingLikeTaskCode                    // 添加待处理点赞任务失败
-	TableIdentifyNotFoundCode                 // 表格不存在
-	TokenGeneratedErrorCode                   // Token 生成失败
 )
 
 var (
+	TokenGeneratedError = func(err error) error {
+		return errorx.New(http.StatusInternalServerError, TokenGeneratedErrorCode, "Token 生成失败", err)
+	}
+	TableIdentifyNotFoundError = func(err error) error {
+		return errorx.New(http.StatusNotFound, TableIdentifyNotFoundCode, "表格标识未找到", err)
+	}
 	RecordNotFoundError = func(err error) error {
 		return errorx.New(http.StatusNotFound, RecordNotFoundCode, "记录不存在", err)
 	}
 	AddPendingLikeTaskError = func(err error) error {
 		return errorx.New(http.StatusInternalServerError, AddPendingLikeTaskCode, "添加待处理点赞任务失败", err)
-	}
-	TableIdentifyNotFoundError = func(err error) error {
-		return errorx.New(http.StatusNotFound, TableIdentifyNotFoundCode, "表格 ID 未找到", err)
-	}
-	TokenGeneratedError = func(err error) error {
-		return errorx.New(http.StatusInternalServerError, TokenGeneratedErrorCode, "Token 生成失败", err)
 	}
 )
